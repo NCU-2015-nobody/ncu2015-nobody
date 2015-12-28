ec2da83b23327d230afac159fabffe207d82e039
tjchen <tzujung2627@gmail.com>
2015-12-28 15:50:18 +0800

    initialize unit test case

---
+++ test/com/nobody/cdc/CDCTest.java
@@ -0,0 +1,38 @@
+package com.nobody.cdc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CDCTest {
+
+    @Test
+    public void testAddVirtualCharacter() throws Exception {
+
+    }
+
+    @Test
+    public void testAddMonster() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateCharacterStatus() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateMonsterThread() throws Exception {
+
+    }
+
+    @Test
+    public void testCharacterAttack() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUpdateInfo() throws Exception {
+
+    }
+}
