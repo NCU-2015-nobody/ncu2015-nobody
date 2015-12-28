014076c5905678d3776ee8d414a2a560e0f52ccf
wind <aszx4510@yahoo.com.tw>
2015-12-29 06:31:13 +0800

    create main

---
+++ src/com/nobody/start/Main.java
@@ -0,0 +1,69 @@
+package com.nobody.start;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		/*
+		 * create uim
+		 * 
+		 * public Server server = new Server();
+		 * public Client client = new Client();
+		 */
+		
+		
+		// startup server or client
+		if(true) // create server
+		{
+			// if user choose create server, enter if
+			
+			/*
+			 * create cdc and cdcsdm
+			 */
+			
+			
+			/*
+			 * create TCP client
+			 */
+			
+			
+			/*
+			 * create UDPbc 
+			 */
+			
+		} // end of create server
+		
+		
+		// create client
+		
+		/*
+		 * create dom
+		 */
+		
+		
+		/*
+		 * create TCP server and startup
+		 */
+		
+		
+		/*
+		 * create UDP server
+		 */
+		
+		
+		/*
+		 * create client frame
+		 * 
+		 * add actionListener
+		 */
+		
+		
+		/*
+		 * create renderEngine
+		 * 
+		 * add three renders
+		 * 
+		 * add render thread
+		 */
+	}
+}
