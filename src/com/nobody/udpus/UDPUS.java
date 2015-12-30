7214f25543dc068f433e1ed11df9e97b8f5ad2e9
Sango <s1003923@go.thu.edu.tw>
2015-12-30 17:46:33 +0800

    aa

--- src/com/nobody/udpus/UDPUS.java
+++ src/com/nobody/udpus/UDPUS.java
@@ -5,13 +5,17 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.nobody.dom.d;
+
 public class UDPUS extends Thread {
 	private int port = 5566;
 	private Socket socket;
 	private ServerSocket serverSocket;
+	private d dom;
 	private boolean flag;
 
-	public UDPUS() {
+	public UDPUS(d dom) {
+		this.dom = dom;
 		flag = true;
 		try {
 			serverSocket = new ServerSocket(port);
@@ -34,19 +38,23 @@ public class UDPUS extends Thread {
 						String data[] = msg.split(",");
 						if (data[0].equals("add")) {
 							if (data[1].equals("chara")) {
-
+								dom.addVirtualCharacter(Integer.valueOf(data[2]), Integer.valueOf(data[3]),
+										Integer.valueOf(data[4]), Integer.valueOf(data[5]), Integer.valueOf(data[6]));
 							}
 							if (data[1].equals("mon")) {
-
+								dom.addMonster(Integer.valueOf(data[2]), Integer.valueOf(data[3]),
+										Integer.valueOf(data[4]), Integer.valueOf(data[5]), Integer.valueOf(data[6]));
 							}
 						}
 
 						if (data[0].equals("update")) {
 							if (data[1].equals("chara")) {
-
+								dom.updateVirtualCharacter(Integer.valueOf(data[2]), Integer.valueOf(data[3]),
+										Integer.valueOf(data[4]), Integer.valueOf(data[5]), Integer.valueOf(data[6]));
 							}
 							if (data[1].equals("mon")) {
-
+								dom.updateMonsterStatus(Integer.valueOf(data[2]), Integer.valueOf(data[3]),
+										Integer.valueOf(data[4]), Integer.valueOf(data[5]), Integer.valueOf(data[6]));
 							}
 						}
 					}
