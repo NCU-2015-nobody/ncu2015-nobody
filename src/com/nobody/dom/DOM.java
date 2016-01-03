b9e51d5809ca80274b0e9c5794aa24f43e1c3753
Teresa <teresasasa13@gmail.com>
2016-01-03 21:13:17 +0800

    minor bug fixed

--- src/com/nobody/dom/DOM.java
+++ src/com/nobody/dom/DOM.java
@@ -154,7 +154,7 @@ public class DOM {
 	public int[] mainCharacterInfo() {
 		int[] character = new int[3];
 		int self_no = client.character();
-		int index = info.clientNumberList.indexOf(self_no);
+		int index = info.clientNumberList.indexOf(self_no-1);
 
 		if (info.CD > 0) {
 			character[0] = 0; // 0 for no attack; 1 for attack
